ae0d072885527d2532cc203982798a730df5d5bc
hugsbord <hugsbord@student.42.fr>
2020-12-30 18:23:40 +0100

    ft_server works fine

--- srcs/init.sh
+++ srcs/init.sh
@@ -1,16 +1,15 @@
-mkdir /var/www/localhost
 mkdir /etc/nginx/ssl
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out /etc/nginx/ssl/localhost.pem -keyout /etc/nginx/ssl/localhost.key -subj "/C=FR/ST=Paris/L=Paris/O=42 School/OU=hugsbord/CN=localhost"
 
 chown -R www-data:www-data /var/www/html/*
 chmod -R 755 /var/www/html/*
+
 service php7.3-fpm start
 service mysql start
 echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
 echo "GRANT ALL ON wordpress.* TO 'root'@'localhost';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
 echo "update mysql.user set plugin='mysql_native_password' where user='root';" | mysql -u root --skip-password
-#service mysql restart
 service nginx start
-#export PATH=$PATH:/home/bin
+
 /bin/bash
